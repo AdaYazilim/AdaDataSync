9e444fe14124b0073204e029e1863cb6a73896d4
Serdar <serdar.suvarierol@gmail.com>
2015-08-25 16:06:05 +0300

    onemsiz

---
+++ AdaDataSync/CalisanServisKontrolcusu.cs
@@ -0,0 +1,17 @@
+﻿using AdaDataSync.API;
+
+namespace AdaDataSync
+{
+    public class CalisanServisKontrolcusu : ICalisanServisKontrolcusu
+    {
+        public bool BuMakinadaBaskaServisCalisiyorMu()
+        {
+            return false;
+        }
+
+        public void MakinaBazindaKilitKoy()
+        {
+
+        }
+    }
+}
