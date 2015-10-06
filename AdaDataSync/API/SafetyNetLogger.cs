abcce130a07ae5961395245889e485bdfaa2a2c5
Serdar <serdar.suvarierol@gmail.com>
2015-10-06 10:11:47 +0300

    Aynı hatayı sürekli vermeye başladığında hata.txt dosyası çok büyüyordu. Bu durumdaki hata mesajı küçültüldü.

--- AdaDataSync/API/SafetyNetLogger.cs
+++ AdaDataSync/API/SafetyNetLogger.cs
@@ -5,12 +5,25 @@ namespace AdaDataSync.API
 {
     class SafetyNetLogger : ISafetyNetLogger
     {
+        private string _sonHataMesaji = string.Empty;
+
         public void HataLogla(Exception exception)
         {
+
             const string path = @"hata.txt";
             using (StreamWriter sw = new StreamWriter(path, true))
             {
-                sw.WriteLine(DateTime.Now.ToString() + " / " + exception.ToString());
+                string hataMesaji = exception.ToString();
+
+                if (hataMesaji == _sonHataMesaji) // belli bir hatayı her seferinde atmaya başladığında hata.txt dosyası büyüyor. 
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + " / Aynısı");
+                }
+                else
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + " / " + hataMesaji);
+                    _sonHataMesaji = hataMesaji;
+                }
             }
         }
     }
