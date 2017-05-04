528cb9712d998a78d6dcb5397aa847acd493347b
Serdar <serdar.suvarierol@gmail.com>
2017-05-04 15:08:16 +0300

    Eposta atma classı eklendi.

---
+++ AdaDataSync/Test/EpostaTest.cs
@@ -0,0 +1,16 @@
+﻿using AdaDataSync.API;
+using NUnit.Framework;
+
+namespace AdaDataSync.Test
+{
+    [TestFixture]
+    class EpostaTest
+    {
+        [Ignore]
+		[Test]
+		public void kaynakta_kayit_yoksa_hedeften_siler()
+		{
+            EPosta.Gonder("deneme", "içerik");
+        }
+	}
+}
